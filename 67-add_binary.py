1733e1aec81e75832d4eb258d5bb6222770ce74f
pubec929 <pubec.308@gmail.com>
2026-07-27 22:03:41 +0200

    solution 67

---
+++ 67-add_binary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return str(bin(int(a, 2) + int(b, 2)))[2:]
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.addBinary("101", "11")
