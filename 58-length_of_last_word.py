a1830219bc71430c8771183e78ebb52703a49a05
pubec929 <pubec.308@gmail.com>
2026-07-28 00:15:45 +0200

    problem 58 solution

---
+++ 58-length_of_last_word.py
@@ -0,0 +1,6 @@
+"""https://leetcode.com/problems/length-of-last-word/description/"""
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        *_, lastWord = s.rstrip().split(" ")
+        return len(lastWord)
