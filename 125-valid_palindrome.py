08d06e0e9129f9f347950d9a8472a6b3d853d53c
pubec929 <pubec.308@gmail.com>
2026-08-06 13:09:01 +0200

    125 first solution

---
+++ 125-valid_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.strip().lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].isalpha() and not s[l].isdigit():
+                l += 1
+                continue
+            if not s[r].isalpha() and not s[r].isdigit():
+                r -= 1
+                continue
+
+            if s[l] != s[r]:
+                return False
+
+            l += 1
+            r -= 1
+        return True
+
+sol = Solution()
+print(sol.isPalindrome("aba"))
