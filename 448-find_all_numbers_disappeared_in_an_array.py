4f487f61293a501392cd7c63c0a03be945e289e4
pubec929 <pubec.308@gmail.com>
2026-08-09 01:06:37 +0200

    448 optimized version

--- 448-find_all_numbers_disappeared_in_an_array.py
+++ 448-find_all_numbers_disappeared_in_an_array.py
@@ -2,22 +2,12 @@
 
 class Solution:
     def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
-        nums.sort()
-        idx = 0
-        expected = list(range(1, len(nums) + 1))
-        missing = []
-        r = 0
-        for idx in range(0, len(nums)):
-            n = nums[idx] 
-            if idx >= 1 and n == nums[idx - 1]:
-                continue
-            if  expected[r] != n:
-                missing.extend(expected[r:n - 1])
-                r = n
-            else:
-                r += 1
-        return missing + expected[r:]
+        setNums = set(nums)
+        missing = [n for n in range(1, len(nums) + 1) if n not in setNums]
+        return missing
+        
+            
 
 sol = Solution()
-nums = [1, 1, 1, 1]
+nums = [4,3,2,7,8,2,3,1]
 print(sol.findDisappearedNumbers(nums))
