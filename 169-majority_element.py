7b0d4a6ab308abae119c38165631d60c861a1f1b
pubec929 <pubec.308@gmail.com>
2026-08-09 21:34:55 +0200

    second version

--- 169-majority_element.py
+++ 169-majority_element.py
@@ -1,14 +1,5 @@
 """https://leetcode.com/problems/majority-element/description/"""
-from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        counter = Counter()
-        l = len(nums) // 2
-        for n in nums:
-            counter[n] += 1
-            if counter[n] > l:
-                return n
-        return -1
-
-print(Solution().majorityElement([3, 2, 3]))
+        return sorted(nums)[len(nums) // 2]
