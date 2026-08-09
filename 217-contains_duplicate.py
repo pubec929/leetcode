6b9d147d929c440802d1519c442f2a7961d6e6a2
pubec929 <pubec.308@gmail.com>
2026-08-09 20:47:23 +0200

    217 final

--- 217-contains_duplicate.py
+++ 217-contains_duplicate.py
@@ -2,11 +2,4 @@
 
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        seen = set()
-        for n in nums:
-            if n in seen:
-                return True
-            seen.add(n)
-        return False
-
-print(Solution().containsDuplicate([1, 2, 3, 4]))
+        return len(nums) != len(set(nums))
