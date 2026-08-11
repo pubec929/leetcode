c1797991e94e1a9f5c429c181fb436da7d904290
pubec929 <pubec.308@gmail.com>
2026-08-11 17:30:45 +0200

    144 iterative version

--- 144-binary_tree_preorder_traversal.py
+++ 144-binary_tree_preorder_traversal.py
@@ -1,5 +1,6 @@
 """https://leetcode.com/problems/binary-tree-preorder-traversal/description/"""
 from typing import Optional
+from collections import deque
 
 class TreeNode:
     def __init__(self, val = 0, left = None, right = None):
@@ -7,13 +8,33 @@ class TreeNode:
         self.left = left
         self.right = right
 
+"""
+recursive 
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
         if root is None:
             return []
 
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+"""
 
+class Solution: 
+    def preorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
+        if root is None:
+            return []
+
+        stack = deque([root])
+        vals = []
+        while stack:
+            node = stack.pop()
+            vals.append(node.val)
+
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+                
+        return vals
 root = TreeNode(1)
 a = TreeNode(2)
 b = TreeNode(3)
