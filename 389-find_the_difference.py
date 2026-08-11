e551a49c404f9147936c5ff5cc86fd5dbf310d51
pubec929 <pubec.308@gmail.com>
2026-08-12 01:04:43 +0200

    389 final solution

--- 389-find_the_difference.py
+++ 389-find_the_difference.py
@@ -2,15 +2,9 @@
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        if len(t) <= 1:
-            return t
-
-        s, t = sorted(s), sorted(t)
-
-        for l, r in zip(s, t):
-            if l != r:
-                return r
-        return t[-1]
-
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+        return chr(result)
 
 print(Solution().findTheDifference("abcd", "abcde"))
